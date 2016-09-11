29f1d71890719befc0ccb956ee0e2787d6a3d079
teramotodaiki <i101323@gmail.com>
2016-09-11 16:51:33 +0900

    render ui

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,14 @@
 import React, {PropTypes, Component} from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+
 
 export default class Main extends Component {
   constructor(props) {
@@ -7,7 +16,10 @@ export default class Main extends Component {
   }
 
   render() {
-    return (<div>MyComponent</div>);
+    return (
+      <MuiThemeProvider>
+      </MuiThemeProvider>
+    );
   }
 }
 
