c18a05a1bc94267fe8c7d1b6d9c5dbb84205db64
teramotodaiki <i101323@gmail.com>
2016-09-12 22:29:22 +0900

    add UI

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,6 +1,6 @@
 import React, {PropTypes, Component} from 'react';
-import {FlatButton} from 'material-ui';
-import SwapHoriz from 'material-ui/svg-icons/action/swap-horiz';
+import {FlatButton, DropDownMenu, MenuItem} from 'material-ui';
+import WebAsset from 'material-ui/svg-icons/av/web-asset';
 import FileDownload from 'material-ui/svg-icons/file/file-download';
 import PowerSettingsNew from 'material-ui/svg-icons/action/power-settings-new';
 
@@ -16,9 +16,8 @@ export default class Menu extends Component {
     };
   }
 
-  swapClicked = (event) => {
-    const {align, setDockAlign} = this.props;
-    setDockAlign(align === 'right' ? 'left' : 'right');
+  handleAlignChange = (event, index, value) => {
+    this.props.setDockAlign(value);
   }
 
   saveClicked = (event) => {
@@ -38,7 +37,9 @@ export default class Menu extends Component {
 
   render() {
     const {openSaveDialog, saveFile} = this.state;
-    const {style, runRequest} = this.props;
+    const {align, style, runRequest} = this.props;
+
+    const rotate = (deg) => ({ transform: `rotate(${deg}deg)` });
 
     return (
       <div style={style}>
@@ -49,7 +50,12 @@ export default class Menu extends Component {
         />
         <FlatButton icon={<PowerSettingsNew />} onClick={runRequest}></FlatButton>
         <FlatButton icon={<FileDownload />} onClick={this.saveClicked}></FlatButton>
-        <FlatButton icon={<SwapHoriz />} onClick={this.swapClicked}></FlatButton>
+        <DropDownMenu value={align} onChange={this.handleAlignChange}>
+          <MenuItem value="top" primaryText="T" leftIcon={<WebAsset style={rotate(0)} />} />
+          <MenuItem value="right" primaryText="R" leftIcon={<WebAsset style={rotate(90)} />} />
+          <MenuItem value="left" primaryText="L" leftIcon={<WebAsset style={rotate(270)} />} />
+          <MenuItem value="bottom" primaryText="B" leftIcon={<WebAsset style={rotate(180)} />} />
+        </DropDownMenu>
       </div>
     );
   }
