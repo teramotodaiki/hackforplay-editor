d5db665db716c0d2018a1859ec8304dfc3d05d5c
teramotodaiki <i101323@gmail.com>
2016-09-12 14:26:57 +0900

    enabled key bind

--- src/SaveDialog.jsx
+++ src/SaveDialog.jsx
@@ -48,6 +48,7 @@ export default class SaveDialog extends Component {
       <FlatButton
         label="Save"
         primary={true}
+        keyboardFocused={true}
         onTouchTap={this.handleSave}
       />
     ];
