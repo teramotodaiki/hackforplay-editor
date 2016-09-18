f0905d72396d97a82b7389f1fcdd4ccb591849fd
teramotodaiki <i101323@gmail.com>
2016-09-18 22:31:53 +0900

    rename file

--- src/Pane.jsx
+++ src/Pane.jsx
@@ -6,6 +6,7 @@ require('codemirror/mode/javascript/javascript');
 require('codemirror/lib/codemirror.css');
 
 import ContextMenu from './ContextMenu';
+import RenameDialog from './RenameDialog';
 
 const PANE_CONTENT_CONTAINER = 'PANE_CONTENT_CONTAINER'; // classname
 
@@ -18,13 +19,12 @@ export default class Pane extends Component {
   };
 
   state = {
-    openContextMenu: {}
+    openContextMenu: {},
+    renameFileIndex: -1
   };
 
   constructor(props) {
     super(props);
-
-    this.updateCode = this.updateCode.bind(this);
   }
 
   getStyle() {
@@ -43,28 +43,42 @@ export default class Pane extends Component {
     addEventListener('resize', setSize);
   }
 
-  updateCode(index, code) {
+  updateCode = (index, code) => {
     const {files, updateFile} = this.props;
     updateFile(index, Object.assign({}, files[index], {code}));
+  }
+
+  updateFilename (index, filename) {
+    const {files, updateFile} = this.props;
+    updateFile(index, Object.assign({}, files[index], {filename}));
   }
 
   handleContextMenu(event, filename) {
     event.preventDefault();
     const { clientX, clientY } = event.nativeEvent;
-    this.setState({
-      openContextMenu: {
-        [filename]: { absX: clientX, absY: clientY }
-      }
-    });
+    const openContextMenu = { filename, absX: clientX, absY: clientY };
+    this.setState({ openContextMenu });
   }
 
   handleContextMenuClose() {
     this.setState({ openContextMenu: {} });
   }
 
+  handleRename = () => {
+    const { files } = this.props;
+    const filename = this.state.openContextMenu.filename;
+    if (!filename) return;
+    const renameFileIndex = files.findIndex((item) => item.filename === filename);
+    this.setState({ renameFileIndex, openContextMenu: {} });
+  }
+
+  handleRenameDialogClose = () => {
+    this.setState({ renameFileIndex: -1 });
+  }
+
   render() {
     const { files } = this.props;
-    const { openContextMenu } = this.state;
+    const { openContextMenu, renameFileIndex } = this.state;
     const options = {
       lineNumbers: true,
       mode: 'javascript'
@@ -77,38 +91,47 @@ export default class Pane extends Component {
 
     const menuList = [
       {
-        primaryText: '--right click menu--'
+        primaryText: 'Rename',
+        onTouchTap: this.handleRename
       }
     ];
 
     return (
-      <Tabs
-        style={style}
-        tabItemContainerStyle={{ flex: '0 0 auto' }}
-        contentContainerStyle={{ flex: '1 1 auto' }}
-        contentContainerClassName={PANE_CONTENT_CONTAINER}
-      >
-      {files.map((file, index) => (
-        <Tab
-          key={file.filename}
-          label={file.filename}
-          style={{ textTransform: 'none' }}
-          onContextMenu={(e) => this.handleContextMenu(e, file.filename)}
+      <div>
+        <RenameDialog
+          open={renameFileIndex > -1}
+          file={renameFileIndex > -1 ? files[renameFileIndex] : null}
+          updateFilename={(filename) => this.updateFilename(renameFileIndex, filename)}
+          onRequestClose={this.handleRenameDialogClose}
+        />
+        <Tabs
+          style={style}
+          tabItemContainerStyle={{ flex: '0 0 auto' }}
+          contentContainerStyle={{ flex: '1 1 auto' }}
+          contentContainerClassName={PANE_CONTENT_CONTAINER}
         >
-          <ContextMenu
-            menuList={menuList}
-            openEvent={openContextMenu[file.filename]}
-            onClose={this.handleContextMenuClose}
-          />
-          <CodeMirror
-            ref={this.setEnoughHeight}
-            value={file.code}
-            onChange={(code) => this.updateCode(index, code)}
-            options={options}
-          />
-        </Tab>
-      ))}
-      </Tabs>
+        {files.map((file, index) => (
+          <Tab
+            key={file.filename}
+            label={file.filename}
+            style={{ textTransform: 'none' }}
+            onContextMenu={(e) => this.handleContextMenu(e, file.filename)}
+          >
+            <ContextMenu
+              menuList={menuList}
+              openEvent={openContextMenu.filename === file.filename ? openContextMenu : null}
+              onClose={this.handleContextMenuClose}
+            />
+            <CodeMirror
+              ref={this.setEnoughHeight}
+              value={file.code}
+              onChange={(code) => this.updateCode(index, code)}
+              options={options}
+            />
+          </Tab>
+        ))}
+        </Tabs>
+      </div>
     );
   }
 }
